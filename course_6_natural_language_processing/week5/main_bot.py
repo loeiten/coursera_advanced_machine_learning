4ff3d7ea2048773cbb8791a2d136150fbd90086f
loeiten <michael.l.magnussen@gmail.com>
2018-11-30 19:22:02 +0100

    Completed main_bot

--- course_6_natural_language_processing/week5/main_bot.py
+++ course_6_natural_language_processing/week5/main_bot.py
@@ -1,50 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+
 import requests
 import time
 import argparse
 import os
 import json
-
+import sys
 from requests.compat import urljoin
 
 
-class BotHandler(object):
-    """
-        BotHandler is a class which implements all back-end of the bot.
-        It has tree main functions:
-            'get_updates' — checks for new messages
-            'send_message' – posts new message to user
-            'get_answer' — computes the most relevant on a user's question
-    """
-
-    def __init__(self, token, dialogue_manager):
-        self.token = token
-        self.api_url = "https://api.telegram.org/bot{}/".format(token)
-        self.dialogue_manager = dialogue_manager
-
-    def get_updates(self, offset=None, timeout=30):
-        params = {"timeout": timeout, "offset": offset}
-        raw_resp = requests.get(urljoin(self.api_url, "getUpdates"), params)
-        try:
-            resp = raw_resp.json()
-        except json.decoder.JSONDecodeError as e:
-            print("Failed to parse response {}: {}.".format(raw_resp.content, e))
-            return []
-
-        if "result" not in resp:
-            return []
-        return resp["result"]
-
-    def send_message(self, chat_id, text):
-        params = {"chat_id": chat_id, "text": text}
-        return requests.post(urljoin(self.api_url, "sendMessage"), params)
-
-    def get_answer(self, question):
-        if question == '/start':
-            return "Hi, I am your project bot. How can I help you today?"
-        return self.dialogue_manager.generate_answer(question)
+if 'UTF-8' not in sys.stdout.encoding:
+    msg = ("Terminal can't handle UTF-8\n\n"
+           "Try exporting:\n"
+           "export LC_ALL=en_US.UTF-8\n"
+           "export LANG=en_US.UTF-8\n"
+           "export LANGUAGE=en_US.UTF-8\n")
+    raise RuntimeError(msg)
 
 
 def parse_args():
@@ -57,70 +30,144 @@ def is_unicode(text):
     return len(text) == len(text.encode())
 
 
+class BotHandler(object):
+    """
+    BotHandler is a class which implements all back-end of the bot.
+
+    Methods
+    -------
+    get_updates(offset=None, timeout=30)
+        Checks for new messages
+    send_message(chat_id, text)
+        Posts new message to user
+    get_answer(question)
+        Computes the most relevant on a user's question
+    """
+
+    def __init__(self, token, dialogue_manager):
+        """
+        Constructor for the bot handler
+
+        Sets token and dialogue_manager
+
+        Parameters
+        ----------
+        token : str
+            The token for the Rest API
+        dialogue_manager : DialogueManager-like
+            The dialogue-manager
+            Must contain the method `generate_answer`
+        """
+        self.token = token
+        self.api_url = 'https://api.telegram.org/bot{}/'.format(token)
+        self.dialogue_manager = dialogue_manager
+
+    def get_updates(self, offset=None, timeout=30):
+        """
+        Fetches updates from the bot
+
+        Parameters
+        ----------
+        offset : None or int
+            Conversation offset, see
+            https://core.telegram.org/bots/api#getupdates
+            for details
+        timeout : int
+            Timeout for long pooling
+
+        Returns
+        -------
+        list
+            The result of the update
+        """
+        params = {'timeout': timeout, 'offset': offset}
+        raw_resp = requests.get(urljoin(self.api_url, 'getUpdates'), params)
+        try:
+            resp = raw_resp.json()
+        except json.decoder.JSONDecodeError as e:
+            print('Failed to parse response {}: {}.'.
+                  format(raw_resp.content, e))
+            return []
+
+        if 'result' not in resp:
+            return []
+        return resp['result']
+
+    def send_message(self, chat_id, text):
+        """
+        Send a message to the bot
+
+        Parameter
+        ---------
+        chat_id : int
+            The chat id to send to
+        text : str
+            The message to send to the bot
+
+        Returns
+        -------
+        Response
+            The response of the post action
+        """
+        params = {'chat_id': chat_id, 'text': text}
+        return requests.post(urljoin(self.api_url, 'sendMessage'), params)
+
+    def get_answer(self, question):
+        """
+        Get the answer to the question
+
+        Parameters
+        ----------
+        question : str
+            The question as a string
+
+        Returns
+        -------
+        str
+            The answer from the dialogue manager
+        """
+        if question == '/start':
+            return 'Hi, I am your project bot. How can I help you today?'
+        return self.dialogue_manager.generate_answer(question)
+
+
 class SimpleDialogueManager(object):
     """
     This is the simplest dialogue manager to test the telegram bot.
     Your task is to create a more advanced one in dialogue_manager.py."
     """
 
-    def generate_answer(self, question):
-        return "Hello, world!"
-
-
-def cast_to_utf_8(old_dict):
-    """
-    Encodes the string content of a dict to utf-8
-
-    Parameters
-    ----------
-    old_dict : dict
-        The dict to encode
-
-    Returns
-    -------
-    new_dict : dict
-        The encoded dict
-    """
-
-    def walk(node):
+    @staticmethod
+    def generate_answer(_):
         """
-        Recursively traverses a node ande encodes all strings to utf-8
+        Replies "Hello, world!" irrespective of the input
 
         Parameters
         ----------
-        node : dict
-            The node to traverse
+        _ : str
+            The input string
 
         Returns
         -------
-        node : dict
-            The node where the strings are encoded to utf-8
+        str
+            The "Hello, world!" string
         """
-        for key, item in node.items():
-            if type(item)==dict:
-                walk(item)
-            elif type(item)==list:
-                for i, elem in enumerate(item):
-                    if type(elem) == str:
-                        node[key][i] = elem.encode('utf-8')
-            elif type(item)==str:
-                node[key] = item.encode('utf-8')
-        return node
-
-    new_dict = walk(old_dict)
-
-    return new_dict
+        return "Hello, world!"
 
 
 def main():
+    """
+    The main function of the conversation service
+    """
     args = parse_args()
     token = args.token
 
     if not token:
-        if not "TELEGRAM_TOKEN" in os.environ:
-            print("Please, set bot token through --token or TELEGRAM_TOKEN env variable")
+        if 'TELEGRAM_TOKEN' not in os.environ:
+            print('Please, set bot token through '
+                  '--token or TELEGRAM_TOKEN env variable')
             return
-        token = os.environ["TELEGRAM_TOKEN"]
+        token = os.environ['TELEGRAM_TOKEN']
 
     #################################################################
 
@@ -135,25 +182,29 @@ def main():
 
     ###############################################################
 
-    print("Ready to talk!")
+    print('Ready to talk!')
     offset = 0
     while True:
         updates = bot.get_updates(offset=offset)
         for update in updates:
-            print("An update received.")
-            if "message" in update:
-                chat_id = update["message"]["chat"]["id"]
-                if "text" in update["message"]:
-                    text = update["message"]["text"]
+            print('An update received.')
+            if 'message' in update:
+                chat_id = update['message']['chat']['id']
+                if 'text' in update['message']:
+                    text = update['message']['text']
                     if is_unicode(text):
-                        update = cast_to_utf_8(update)
-                        print("Update content: {}".format(update))
-                        bot.send_message(chat_id, bot.get_answer(update["message"]["text"]))
+                        print('Update content: {}'.format(update))
+                        bot.send_message(chat_id,
+                                         bot.get_answer(
+                                             update['message']['text']))
                     else:
-                        bot.send_message(chat_id, "Hmm, you are sending some weird characters to me...")
+                        msg = ('Hmm, you are sending some weird characters to '
+                               'me...')
+                        bot.send_message(chat_id, msg)
             offset = max(offset, update['update_id'] + 1)
         time.sleep(1)
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
 
