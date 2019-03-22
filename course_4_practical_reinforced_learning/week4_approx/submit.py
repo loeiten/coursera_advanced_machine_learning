943403ec7c1a3c56f2f7a2ab9b967cd759460007
loeiten <michael.l.magnussen@gmail.com>
2019-03-22 14:21:28 +0100

    Completed assignment

--- course_4_practical_reinforced_learning/week4_approx/submit.py
+++ course_4_practical_reinforced_learning/week4_approx/submit.py
@@ -14,9 +14,7 @@ def submit_cartpole(generate_session, email, token):
     grader.submit(email, token)
 
 
-def submit_breakout(agent, env, evaluate, email, token):
-    sessions = [evaluate(env, agent, n_games=1) for _ in range(100)]
-    session_rewards = np.array(sessions)
+def submit_breakout(session_rewards, email, token):
     grader = grading.Grader("WTOZHCn1EeiNwAoZNi-Hrg")
     grader.set_answer("VFM7Z", int(np.mean(session_rewards)))
     grader.submit(email, token)
