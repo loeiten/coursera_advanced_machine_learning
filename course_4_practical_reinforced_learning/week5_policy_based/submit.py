33ecdadf3ce224c7c93a59e5153e38826618133a
loeiten <michael.l.magnussen@gmail.com>
2019-04-01 17:18:46 +0200

    Bugfix to submit

--- course_4_practical_reinforced_learning/week5_policy_based/submit.py
+++ course_4_practical_reinforced_learning/week5_policy_based/submit.py
@@ -16,5 +16,8 @@ def submit_kungfu(agent, env, evaluate, email, token):
     sessions = [evaluate(agent=agent, env=env, n_games=1) for _ in range(100)]
     session_rewards = np.array(sessions)
     grader = grading.Grader("6sPnVCn6EeieSRL7rCBNJA")
-    grader.set_answer("HhNVX", int(np.mean(session_rewards)))
+    # NOTE: Bug, so multiplied with 100, see
+    #       https://www.coursera.org/learn/practical-rl/programming/95h2n/grader-a3c/discussions/threads/Q1XYCe08EeigTgpIFoGjKg
+    #       https://www.coursera.org/learn/practical-rl/programming/95h2n/grader-a3c/discussions/threads/F1JHgGRmEei8iRK3RpIYPA/replies/vILx4WchEeimAgoMPemTPA
+    grader.set_answer("HhNVX", 100*int(np.mean(session_rewards)))
     grader.submit(email, token)
